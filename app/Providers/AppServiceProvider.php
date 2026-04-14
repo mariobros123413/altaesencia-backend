d9816d1e525defdf24d71db6faa44899979a0046
mariobros123413 <j.mario18@hotmail.es>
2026-04-13 22:19:38 -0400

    conf. deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+
+        $appUrl = rtrim((string) config('app.url'), '/');
+        $forceHttps = filter_var(env('FORCE_HTTPS', false), FILTER_VALIDATE_BOOL)
+            || ($this->app->environment('production') && str_starts_with($appUrl, 'https://'));
+
+        if ($forceHttps) {
+            URL::forceHttps();
+
+            if ($appUrl !== '') {
+                URL::useOrigin($appUrl);
+                URL::useAssetOrigin($appUrl);
+            }
+        }
     }
 }
