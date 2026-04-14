546189c5ef6f3a19fd7707aaf8b78cdfcf00db23
mariobros123413 <j.mario18@hotmail.es>
2026-04-13 23:07:36 -0400

    cors: agregacion cors

--- config/cors.php
+++ config/cors.php
@@ -15,6 +15,7 @@ return [
         'http://localhost:5174',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:5174',
+        'https://altaesencia.lat'
     ]),
 
     'allowed_origins_patterns' => [],
